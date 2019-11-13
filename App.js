e3b9bbdcbb94e16711560446ed26ecd068d820e9
Luke Feely <lfeely@tcd.ie>
2019-11-13 20:19:19 +0100

    Trying to retrieve info

--- App.js
+++ App.js
@@ -1,1 +1,24 @@
 Test
+import React, { Component } from 'react';
+import axios from "axios";
+import Popup from "reactjs-popup";
+import './App.css';
+import UserForm from "./components/UserForm";
+import styled from 'styled-components';
+import Chart from './components/Charts';
+import PieChart from './components/PieChart';
+
+
+
+
+class App extends Component {
+  state = {
+    name: null,
+    id: null,
+    avatar: null,
+    followers: null,
+    repos: [],
+    languages: [],
+    chartData: [],
+    pieChartData:[]
+  }
